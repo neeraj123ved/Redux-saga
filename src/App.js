381909ff0cb559aae78f1df4ab57092980f09040
neeraj123ved <neeraj.ved@gmail.com>
2022-09-04 17:18:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './component/Header';
+import { Main } from './component/Main';
+import { Cart } from './component/Cart';
 
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path={'/'} element={<Main />} />
+        <Route path={'/cart'} element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
